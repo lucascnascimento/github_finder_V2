283c572d314b3274bad5d937dd78abbb6c82a61b
Lucas Cordeiro <lucascnascimento@gmail.com>
2019-10-29 21:00:53 -0300

    App first refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from "./Components/layout/Navbar"
+import UserItem from "./Components/Users/UserItem"
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className = "App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <UserItem />
       </div>
     );
   } 
